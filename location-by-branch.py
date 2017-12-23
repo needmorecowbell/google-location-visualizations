2deb0ce191a774ce21575623dd153b80ed609492
Adam Musciano <amusciano@gmail.com>
2017-12-23 01:01:54 -0500

    Added date progression label and worked on formatting

--- location-by-branch.py
+++ location-by-branch.py
@@ -1,27 +1,33 @@
 import pandas as pd
 from datetime import datetime as dt
-import json, time, pyglet
+import json, time, pyglet, sys
 
 class LocationBrancher():
     """docstring for LocationBrancher."""
 
     df_gps= None
     date_range=None
-
-    window= None
-    label = None
     locations = []
 
+    # GUI Components
+    window_size= [1000,800]
+    window= None
+    label = None
+    time_label= None
     fps_display= pyglet.clock.ClockDisplay()
 
+
     def __init__(self):
-        self.window= pyglet.window.Window(visible=False)
+        self.window= pyglet.window.Window( self.window_size[0], self.window_size[1], visible=False, resizable=False)
 
         self.label = pyglet.text.Label('Location Brancher',
                                     font_size=42,
                                     x=self.window.width//2 , y=self.window.height//2,
                                     anchor_x='center', anchor_y='center')
-
+        self.time_label = pyglet.text.Label("Current Date:  ",
+                                    font_size=16,
+                                    x= (self.window.height//8) , y=self.window.height//8,
+                                    anchor_x='center', anchor_y='center')
         self.on_draw = self.window.event(self.on_draw) #instead of decorator
 
         print("Loading History....")
@@ -40,8 +46,8 @@ class LocationBrancher():
 
 
     def _fillLocationStack(self):
-
-        for x in range(0,len(self.df_gps['datetime'].keys() ) ): # for every element in df_gps...
+        limit= int(len(self.df_gps['datetime'].keys() ) // 1000) # number of points to acquire (1/4 total)
+        for x in range(0, limit): # for every element in df_gps...
             if(-1 != int(self.df_gps['alt'][x]) ): #if we didn't set the value to -1 (meaning there is no altitude data for that point)
 
                 print("Location: "+str(self.df_gps['lat'][x])+", "+str(self.df_gps['lon'][x]))
@@ -53,32 +59,40 @@ class LocationBrancher():
                 lat = self.df_gps['lat'][x]
                 lon = self.df_gps['lon'][x]
                 timestamp = self.df_gps['datetime'][x]
-
-
-                self.locations.append({"alt":alt,"lat":lat,"lon":lon,"time:":timestamp}) # stack the dict on to the top
+                self.locations.append({"alt":alt,"lat":lat,"lon":lon,"time":timestamp}) # stack the dict on to the top
 
 
 
     def _loadPoint(self):
-        location = self.locations.pop()
-        if location is not None:
+        if(len(self.locations)>0):
+            location = self.locations.pop()
+
             self.label = pyglet.text.Label("Alt: "+str(location['alt'])+"    Lat: "+str(location['lat'])+"    Lon: "+str(location['lon']),
                                         font_size=16,
                                         x= self.window.height//2 , y=self.window.height//2,
-                                        anchor_x='center', anchor_y='center')
+                                        align='center',
+                                        anchor_x='center',anchor_y='center')
+            self.time_label = pyglet.text.Label("Date:  "+str(location['time']),
+                                        font_size=12,
+                                        x= 10 , y=10,
+                                        align="left")
         else:
-            print("No more Points")
-            return -1
+            print("Stack has been exhausted")
+
 
     def _reloadDisplay(self):
         self.window.clear()
         self.label.draw()
-        self.fps_display.draw()
+        self.time_label.draw()
+        #self.fps_display.draw()
 
 
     def on_draw(self):
         self._reloadDisplay()
-        self._loadPoint()
+
+        if(len(self.locations) >0):
+            self._loadPoint()
+            print("Points Left: "+str(len(self.locations)))
 
     def __loadHistory(self):
         # load the full location history json file downloaded from google
